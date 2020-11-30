b80e822132cae39d8e29027415c0d512bea26a71
thibault <thibault.bouzat@essec.edu>
2020-11-30 18:12:16 +0800

    deployment Rinkeby

--- README.md
+++ README.md
@@ -111,7 +111,7 @@ All tests should be passed!
 
 - [x] Have at least 5 tests for each smart contract
 
-- [] Smart contract should be deployed to a testnet
+- [x] Smart contract should be deployed to a testnet
 
 #### Frontend
 - [x] Have a development server to serve the frontend interaction of the application locally (You should be able to visit a local URL and interact with the application)
@@ -134,6 +134,6 @@ All tests should be passed!
 
 - [x] A document called avoiding_common_attacks.md explaining security steps you took what measures you took to ensure your contracts are not susceptible to common attacks AND
 
-- [] A document called deployed_addresses.txt that describes where your contracts live (testnet AND address).
+- [x] A document called deployed_addresses.txt that describes where your contracts live (testnet AND address).
 
 - [] A screen recording walking through your Dapp.
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -59,21 +61,20 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
+    rinkeby: {
+     provider: () =>
+       new HDWalletProvider(
+         process.env.MNEMONIC,
+         `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+       ),
+     network_id: 4, // Rinkeby's id
+     gas: 5500000, // Rinkeby has a lower block limit than mainnet
+     confirmations: 2, // # of confs to wait between deployments. (default: 0)
+     timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+     skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+   },
+ },
+
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
